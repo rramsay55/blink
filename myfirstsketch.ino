40281e19b8fd90d32db8a897b9a2137381c3e752
rramsay55 <98988883+rramsay55@users.noreply.github.com>
2022-03-23 16:43:53 -0600

    Add files via upload
     Changed LED pin from 13  to 20

--- myfirstsketch.ino
+++ myfirstsketch.ino
@@ -2,15 +2,15 @@
 // the setup function runs once when you press reset or power the board
 
 void setup() {
-  pinMode(13, OUTPUT);
+  pinMode(10, OUTPUT);
 }
 
 // the loop function runs over and over again forever
 
 void loop() {
   
-  digitalWrite(13, HIGH);   // turn the LED on (HIGH is the voltage level)
+  digitalWrite(10, HIGH);   // turn the LED on (HIGH is the voltage level)
   delay(1000);                       // wait for a second
- digitalWrite(13, LOW);    // turn the LED off by making the voltage LOW
+ digitalWrite(10, LOW);    // turn the LED off by making the voltage LOW
   delay(1000);                       // wait for a second
 }
